e40e16f55faf8ce04bb44fbdb0237193d213697c
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-11-28 00:51:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
