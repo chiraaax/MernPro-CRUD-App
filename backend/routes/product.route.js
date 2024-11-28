46f64936ae4ae6ba598aa3eb609935cc9cf7bcae
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-11-28 07:08:38 +0530

    Completed-product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,2 +1,12 @@
 import express from "express";
 import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+
+router.get("/", getProducts);// GET all products
+router.post("/", createProduct);// POST a new product
+router.put("/:id", updateProduct);// PUT (update) a product by ID
+router.delete("/:id", deleteProduct);// DELETE a product by ID
+
+export default router;
