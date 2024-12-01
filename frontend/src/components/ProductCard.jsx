d664fae3365ee4aeb3a0d3f1c0431bc71fd7480c
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-12-01 23:38:55 +0530

    Update ProductCard.jsx

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,1 +1,26 @@
-
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Tag,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+  useToast,
+  VStack,
+  Tooltip,
+} from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
+import { useState } from "react";
